b9a04f0e622c33b8affdbb6a51e5dd0cb3929921
Mehran Sattarzadeh <54243372+MehranSattar@users.noreply.github.com>
2024-07-11 00:14:15 +0100

    Added scroll bar to provably fair popup

--- apps/frontend/src/app/cases/[id]/components/ProvablyFair.tsx
+++ apps/frontend/src/app/cases/[id]/components/ProvablyFair.tsx
@@ -6,7 +6,7 @@ interface ProvablyFairProps {
   clientSeed: string;
   rollValue: number | null;
   serverSeed: string | null;
-  previousServerSeedHash: string | null
+  previousServerSeedHash: string | null;
   onClientSeedChange: (newClientSeed: string) => void;
 }
 
@@ -16,7 +16,7 @@ export const ProvablyFair: React.FC<ProvablyFairProps> = ({
   rollValue,
   serverSeed,
   previousServerSeedHash,
-  onClientSeedChange
+  onClientSeedChange,
 }) => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [localClientSeed, setLocalClientSeed] = useState(clientSeed);
@@ -34,19 +34,22 @@ export const ProvablyFair: React.FC<ProvablyFairProps> = ({
     onClientSeedChange(localClientSeed);
   }, [localClientSeed, onClientSeedChange]);
 
-  const handleKeyPress = useCallback((event: KeyboardEvent) => {
-    if (event.key === 'Escape') {
-      closePopup();
-    }
-  }, [closePopup]);
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        closePopup();
+      }
+    },
+    [closePopup]
+  );
 
   useEffect(() => {
     if (isPopupOpen) {
-      document.addEventListener('keydown', handleKeyPress);
+      document.addEventListener("keydown", handleKeyPress);
     }
 
     return () => {
-      document.removeEventListener('keydown', handleKeyPress);
+      document.removeEventListener("keydown", handleKeyPress);
     };
   }, [isPopupOpen, handleKeyPress]);
 
@@ -56,15 +59,20 @@ export const ProvablyFair: React.FC<ProvablyFairProps> = ({
 
   return (
     <div className="w-full h-auto">
-      <p className="inline-block text-white cursor-pointer px-2 py-1" onClick={handleProvablyFairClick}>
+      <p
+        className="inline-block text-white cursor-pointer px-2 py-1"
+        onClick={handleProvablyFairClick}
+      >
         Provably Fair
       </p>
       {isPopupOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-background p-8 rounded-lg shadow-lg w-11/12 max-w-4xl">
+          <div className="bg-background p-8 rounded-lg shadow-lg w-11/12 max-w-4xl h-4/5 overflow-y-auto">
             <h2 className="text-2xl font-bold mb-6 text-white">Provably Fair System</h2>
             <div className="mb-6">
-              <label className="block text-sm font-medium text-gray-300 mb-2">Current Server Seed Hash</label>
+              <label className="block text-sm font-medium text-gray-300 mb-2">
+                Current Server Seed Hash
+              </label>
               <input
                 type="text"
                 value={serverSeedHash}
@@ -73,29 +81,35 @@ export const ProvablyFair: React.FC<ProvablyFairProps> = ({
               />
             </div>
             <div className="mb-6">
-              <label className="block text-sm font-medium text-gray-300 mb-2">Previous Server Seed Hash</label>
+              <label className="block text-sm font-medium text-gray-300 mb-2">
+                Previous Server Seed Hash
+              </label>
               <input
                 type="text"
-                value={previousServerSeedHash || 'N/A'}
+                value={previousServerSeedHash || "N/A"}
                 readOnly
                 className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-not-allowed"
               />
-          </div>
+            </div>
             <div className="mb-6">
-              <label className="block text-sm font-medium text-gray-300 mb-2">Previous Server Seed Unhashed</label>
+              <label className="block text-sm font-medium text-gray-300 mb-2">
+                Previous Server Seed Unhashed
+              </label>
               <input
                 type="text"
                 value={
-                  serverSeed !== null && typeof serverSeed === 'number'
+                  serverSeed !== null && typeof serverSeed === "number"
                     ? serverSeed
-                    : serverSeed || 'N/A'
+                    : serverSeed || "N/A"
                 }
                 readOnly
                 className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-not-allowed"
               />
             </div>
             <div className="mb-6">
-              <label className="block text-sm font-medium text-gray-300 mb-2">Client Seed</label>
+              <label className="block text-sm font-medium text-gray-300 mb-2">
+                Client Seed
+              </label>
               <input
                 type="text"
                 value={localClientSeed}
@@ -104,13 +118,15 @@ export const ProvablyFair: React.FC<ProvablyFairProps> = ({
               />
             </div>
             <div className="mb-6">
-              <label className="block text-sm font-medium text-gray-300 mb-2">Roll Value</label>
+              <label className="block text-sm font-medium text-gray-300 mb-2">
+                Roll Value
+              </label>
               <input
                 type="text"
                 value={
-                  rollValue !== null && typeof rollValue === 'number'
+                  rollValue !== null && typeof rollValue === "number"
                     ? rollValue
-                    : rollValue || 'N/A'
+                    : rollValue || "N/A"
                 }
                 readOnly
                 className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 cursor-not-allowed"
@@ -119,16 +135,18 @@ export const ProvablyFair: React.FC<ProvablyFairProps> = ({
             <div className="mb-6">
               <h3 className="text-lg font-semibold mb-2 text-white">How it works</h3>
               <p className="text-gray-300">
-                Our provably fair system ensures that the outcome of each case opening is random and verifiable. 
-                The server seed hash and client seed are combined to generate a unique outcome for each spin.
-                The roll value represents the result of this combination, determining the item you receive.
+                Our provably fair system ensures that the outcome of each case opening is random and
+                verifiable. The server seed hash and client seed are combined to generate a unique
+                outcome for each spin. The roll value represents the result of this combination,
+                determining the item you receive.
               </p>
             </div>
             <div className="mb-6">
               <h3 className="text-lg font-semibold mb-2 text-white">Verify Your Results</h3>
               <p className="text-gray-300">
-                You can use these seeds and the roll value to independently verify the fairness of your case openings. 
-                Visit our verification page to learn more about the process and try it yourself.
+                You can use these seeds and the roll value to independently verify the fairness of
+                your case openings. Visit our verification page to learn more about the process and
+                try it yourself.
               </p>
             </div>
             <button
