ece914a7a0a7652bc3eca8dc6bdc0d050d4bbe30
armansattarzadeh <armansattarzadeh@hotmail.com>
2024-07-18 21:59:54 +0100

    feat: wip

--- apps/frontend/src/app/cases/[id]/hooks/useFetchImage.ts
+++ apps/frontend/src/app/cases/[id]/hooks/useFetchImage.ts
@@ -4,12 +4,16 @@ export const useFetchImage = (url: string) => {
   return useQuery({
     queryKey: ["image", url],
     queryFn: async () => {
-      const response = await fetch(url);
+      const response = await fetch(url, {
+        cache: "force-cache",
+      });
       if (!response.ok) {
         throw new Error("Failed to fetch image");
       }
       const blob = await response.blob();
       return URL.createObjectURL(blob);
     },
+    staleTime: Infinity,
+    gcTime: Infinity,
   });
 };
