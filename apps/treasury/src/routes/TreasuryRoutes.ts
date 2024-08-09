69321a89b407d2f42d0bd65d9b772e2f0a02f534
armansattarzadeh <armansattarzadeh@hotmail.com>
2024-08-09 12:26:38 +0100

    feat: Deleted redundant folders

--- apps/treasury/src/routes/TreasuryRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { TreasuryController } from '../controllers/TreasuryController';
-
-const router = express.Router();
-const treasuryController = new TreasuryController();
-router.post('/deposit/:userId', treasuryController.deposit);
-router.post('/withdraw/:userId', treasuryController.withdraw);
-
-export default router;
